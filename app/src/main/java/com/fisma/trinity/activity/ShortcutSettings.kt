1c4b7451f7c3a89a461971893086da450c5c240a
muhammadsayuti <muhammad.sayuti94@gmail.com>
2019-10-20 15:20:05 +0800

    move AvailableShortcutAdapter to ShortcutAdapter.kt file

--- app/src/main/java/com/fisma/trinity/activity/ShortcutSettings.kt
+++ app/src/main/java/com/fisma/trinity/activity/ShortcutSettings.kt
@@ -1,27 +1,44 @@
 package com.fisma.trinity.activity
 
+import android.bluetooth.BluetoothAdapter
+import android.content.pm.PackageManager
 import android.graphics.Color
-import android.os.Build
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
+import com.fisma.trinity.Constants
 import com.fisma.trinity.R
-import com.fisma.trinity.adapter.DashboardShortcutAdapter
+import com.fisma.trinity.TrinityPluginProvider
+import com.fisma.trinity.adapter.ShortcutAdapter
 import com.fisma.trinity.model.ShortcutItem
 import com.fisma.trinity.util.ImageUtil
 import com.fisma.trinity.util.LauncherAction
+import com.fisma.trinity.util.Tool
+import com.fisma.trinity.viewutil.IconLabelItem
+import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter
+import com.turingtechnologies.materialscrollbar.INameableAdapter
 import com.woxthebox.draglistview.DragListView
 
 class ShortcutSettings : ThemeActivity() {
   var toolbar: Toolbar? = null
-  internal var mAddedShortcuts: ArrayList<ShortcutItem> = ArrayList()
+  var mAddedShortcuts: ArrayList<ShortcutItem> = ArrayList()
+  var mAvailableShortcuts: ArrayList<ShortcutItem> = ArrayList()
 
   val mAddedShortcutsGrid: DragListView
     get() = findViewById(R.id.added_shortcuts)
+  val mAvailableShortcutsGrid: RecyclerView
+    get() = findViewById(R.id.builtin_shortcuts)
+
+  companion object {
+    const val TAG = "ShortcutSettings"
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -30,56 +47,14 @@ class ShortcutSettings : ThemeActivity() {
     toolbar = findViewById(R.id.toolbar)
     toolbar!!.title = "Shortcut Settings"
     setSupportActionBar(toolbar)
-
-//    var item = ShortcutItem()
-//    item.type = ShortcutItem.Type.ACTION
-//    item.action = LauncherAction.Action.LauncherSettings
-//    item.label = "Settings"
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-//      item.icon = ImageUtil.drawableToBitmap(resources.getDrawable(R.drawable.ic_settings_launcher_black_24dp, theme))
-//    else
-//      item.icon = ImageUtil.drawableToBitmap(resources.getDrawable(R.drawable.ic_settings_launcher_black_24dp))
-//
-//    mAddedShortcuts.add(item)
-//
-//    item = ShortcutItem()
-//    item.action = LauncherAction.Action.SetWallpaper
-//    item.type = ShortcutItem.Type.ACTION
-//    item.label = "Wallpaper"
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-//      item.icon = ImageUtil.drawableToBitmap(resources.getDrawable(R.drawable.ic_photo_black_24dp, theme))
-//    else
-//      item.icon = ImageUtil.drawableToBitmap(resources.getDrawable(R.drawable.ic_photo_black_24dp))
-//
-//    mAddedShortcuts.add(item)
-//
-//    item = ShortcutItem()
-//    item.action = LauncherAction.Action.Bluetooth
-//    item.type = ShortcutItem.Type.ACTION
-//    item.label = "Bluetooth"
-//    var drawable = VectorDrawableCompat.create(resources, R.drawable.ic_bluetooth, theme)
-//    DrawableCompat.setTint(drawable!!, Color.parseColor("#FFFFFF"))
-//    item.icon = ImageUtil.drawableToBitmap(drawable)
-//
-//    mAddedShortcuts.add(item)
-//
-//    item = ShortcutItem()
-//    item.action = LauncherAction.Action.Flashlight
-//    item.type = ShortcutItem.Type.ACTION
-//    item.label = "Flashlight"
-//    drawable = VectorDrawableCompat.create(resources, R.drawable.ic_flashlight, theme)
-//    DrawableCompat.setTint(drawable!!, Color.parseColor("#FFFFFF"))
-//    item.icon = ImageUtil.drawableToBitmap(drawable)
-//
-//    mAddedShortcuts.add(item)
-
     mAddedShortcuts = HomeActivity._db.shortcuts
 
     for (shortcut in mAddedShortcuts) {
       updateIconForTheme(shortcut)
     }
 
-    val adapter = DashboardShortcutAdapter(mAddedShortcuts, R.layout.shortcut_item, R.id.item_layout)
+    val adapter = ShortcutAdapter(mAddedShortcuts, R.layout.shortcut_item, R.id.item_layout)
+
     mAddedShortcutsGrid.setAdapter(adapter, false)
 
     mAddedShortcutsGrid.setDragListListener(object : DragListView.DragListListener {
@@ -88,26 +63,147 @@ class ShortcutSettings : ThemeActivity() {
       }
 
       override fun onItemDragStarted(position: Int) {
+        val item = mAddedShortcuts[position]
+        Log.d(TAG, "start dragging item ${item.label}")
         Toast.makeText(this@ShortcutSettings, "Start - position: " + position, Toast.LENGTH_SHORT).show()
       }
 
       override fun onItemDragEnded(fromPosition: Int, toPosition: Int) {
         if (fromPosition != toPosition) {
+          val item = mAddedShortcuts[toPosition]
+          Log.d(TAG, "finish dragging item ${item.label}")
           Toast.makeText(this@ShortcutSettings, "End - position: " + toPosition, Toast.LENGTH_SHORT).show()
         }
       }
     })
 
     mAddedShortcutsGrid.setLayoutManager(GridLayoutManager(this, 5))
+    mAddedShortcutsGrid.overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+    mAvailableShortcutsGrid.layoutManager = GridLayoutManager(this, 5)
+    mAvailableShortcutsGrid.overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+    initAvailableShortcuts()
+  }
+
+  override fun onStop() {
+    super.onStop()
+    Log.d(TAG, "onStop()")
+    for (i in 0 until mAddedShortcuts.size) {
+      val item = mAddedShortcuts[i]
+      item.index = i
+      HomeActivity._db.saveShortcut(item)
+    }
+    TrinityPluginProvider.getInstance()?.updateShortcuts()
   }
 
   fun initAvailableShortcuts() {
+    mAvailableShortcuts.clear()
+    val shortcuts = HashMap<String, ShortcutItem>()
+    for (shortcut in mAddedShortcuts) {
+      shortcuts[shortcut.label!!] = shortcut
+    }
 
+    var shortcut: ShortcutItem
+
+    val hasBluetoothFeature = BluetoothAdapter.getDefaultAdapter() != null
+    if (!shortcuts.containsKey("Bluetooth") && hasBluetoothFeature) {
+      shortcut = ShortcutItem.Builder()
+        .setType(ShortcutItem.Type.ACTION)
+        .setAction(LauncherAction.Action.Bluetooth)
+        .setLabel("Bluetooth")
+        .build()
+      updateIconForTheme(shortcut)
+      mAvailableShortcuts.add(shortcut)
+    }
+
+    val flashlightAvailable = packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
+    if (flashlightAvailable && !shortcuts.containsKey("Flashlight")) {
+      shortcut = ShortcutItem.Builder()
+        .setType(ShortcutItem.Type.ACTION)
+        .setAction(LauncherAction.Action.Flashlight)
+        .setLabel("Flashlight")
+        .build()
+      updateIconForTheme(shortcut)
+      mAvailableShortcuts.add(shortcut)
+    }
+
+    if (!shortcuts.containsKey("Wallpaper")) {
+      shortcut = ShortcutItem.Builder()
+        .setType(ShortcutItem.Type.ACTION)
+        .setAction(LauncherAction.Action.SetWallpaper)
+        .setLabel("Wallpaper")
+        .build()
+      updateIconForTheme(shortcut)
+      mAvailableShortcuts.add(shortcut)
+    }
+
+    if (!shortcuts.containsKey("Settings")) {
+      shortcut = ShortcutItem.Builder()
+        .setType(ShortcutItem.Type.ACTION)
+        .setAction(LauncherAction.Action.LauncherSettings)
+        .setLabel("Settings")
+        .build()
+      updateIconForTheme(shortcut)
+      mAvailableShortcuts.add(shortcut)
+    }
+    if (!shortcuts.containsKey("Messages")) {
+      val info = Tool.getDefaultAppInfo(packageManager, Constants.AppCategory.MESSAGING)
+      shortcut = ShortcutItem.Builder()
+        .setType(ShortcutItem.Type.APP)
+        .setPackageName(info!!.activityInfo.packageName)
+        .setClassName(info!!.activityInfo.name)
+        .setAction(LauncherAction.Action.LauncherSettings)
+        .setIcon(info.loadIcon(packageManager))
+        .setLabel("Messages")
+        .build()
+      mAvailableShortcuts.add(shortcut)
+    }
+
+    if (!shortcuts.containsKey("Phone")) {
+      val info = Tool.getDefaultAppInfo(packageManager, Constants.AppCategory.PHONE)
+      shortcut = ShortcutItem.Builder()
+        .setType(ShortcutItem.Type.APP)
+        .setPackageName(info!!.activityInfo.packageName)
+        .setClassName(info!!.activityInfo.name)
+        .setAction(LauncherAction.Action.LauncherSettings)
+        .setIcon(info.loadIcon(packageManager))
+        .setLabel("Phone")
+        .build()
+      mAvailableShortcuts.add(shortcut)
+    }
+
+    updateAvailableShortcuts()
+  }
+
+  fun updateAvailableShortcuts() {
+    val items = ArrayList<IconLabelItem>()
+    for (shortcut in mAvailableShortcuts) {
+      val item = IconLabelItem(BitmapDrawable(resources, shortcut.icon), shortcut.label!!)
+        .withIconSize(this, 48)
+        .withIconPadding(this, 5)
+        .withTextGravity(Gravity.CENTER)
+        .withTextSize(10f)
+        .withIconGravity(Gravity.TOP)
+        .withIsAppLauncher(false)
+      items.add(item)
+    }
+
+
+    val adapter = AvailableShortcutAdapter()
+    mAvailableShortcutsGrid.adapter = adapter
+    adapter.withOnClickListener { v, adapter, item, position ->
+      val removed = mAvailableShortcuts.removeAt(position)
+      mAddedShortcuts.add(removed)
+      mAvailableShortcuts
+      updateAvailableShortcuts()
+      mAddedShortcutsGrid.adapter.notifyDataSetChanged()
+      false
+    }
+    adapter.set(items)
   }
 
   fun updateIconForTheme(shortcut: ShortcutItem) {
     val attrs = IntArray(1)
-    attrs[0] = R.attr.icon_color
+    attrs[0] = R.styleable.DashboardView_iconColor
 
     val typedArray = theme.obtainStyledAttributes(attrs)
     val color = typedArray.getColor(0, Color.WHITE)
@@ -115,23 +211,32 @@ class ShortcutSettings : ThemeActivity() {
       LauncherAction.Action.Bluetooth -> {
         val drawable = VectorDrawableCompat.create(resources, R.drawable.ic_bluetooth, theme)
         DrawableCompat.setTint(drawable!!, color)
-        shortcut.icon = ImageUtil.drawableToBitmap(drawable)
+        shortcut.iconTheme = ImageUtil.drawableToBitmap(drawable)
       }
       LauncherAction.Action.Flashlight -> {
         val drawable = VectorDrawableCompat.create(resources, R.drawable.ic_flashlight, theme)
         DrawableCompat.setTint(drawable!!, color)
-        shortcut.icon = ImageUtil.drawableToBitmap(drawable)
+        shortcut.iconTheme = ImageUtil.drawableToBitmap(drawable)
       }
       LauncherAction.Action.LauncherSettings -> {
         val drawable = VectorDrawableCompat.create(resources, R.drawable.ic_launcher_settings, theme)
         DrawableCompat.setTint(drawable!!, color)
-        shortcut.icon = ImageUtil.drawableToBitmap(drawable)
+        shortcut.iconTheme = ImageUtil.drawableToBitmap(drawable)
       }
       LauncherAction.Action.SetWallpaper -> {
         val drawable = VectorDrawableCompat.create(resources, R.drawable.ic_photo_black_24dp, theme)
         DrawableCompat.setTint(drawable!!, color)
-        shortcut.icon = ImageUtil.drawableToBitmap(drawable)
+        shortcut.iconTheme = ImageUtil.drawableToBitmap(drawable)
       }
+    }
+  }
+
+  inner class AvailableShortcutAdapter : FastItemAdapter<IconLabelItem>(), INameableAdapter {
+    override fun getCharacterForElement(element: Int): Char? {
+      return if (element < mAvailableShortcuts.size && mAvailableShortcuts!![element] != null && mAvailableShortcuts[element].label!!.length > 0)
+        mAvailableShortcuts[element].label!![0]
+      else
+        '#'
     }
   }
 }
