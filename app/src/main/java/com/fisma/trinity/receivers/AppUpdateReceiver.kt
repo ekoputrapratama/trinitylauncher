3e9b63662eb52230479a3bdd488540b30fc1b3b3
muhammadsayuti <muhammad.sayuti94@gmail.com>
2019-10-23 03:21:34 +0800

    reload plugins on package changed

--- app/src/main/java/com/fisma/trinity/receivers/AppUpdateReceiver.kt
+++ app/src/main/java/com/fisma/trinity/receivers/AppUpdateReceiver.kt
@@ -3,11 +3,22 @@ package com.fisma.trinity.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.fisma.trinity.manager.PluginManager
 import com.fisma.trinity.manager.Settings
 
 
 class AppUpdateReceiver : BroadcastReceiver() {
+  companion object {
+    const val TAG = "AppUpdateReceiver"
+  }
+
   override fun onReceive(context: Context, intent: Intent) {
     Settings.appLoader().onAppUpdated(context, intent)
+    val pm = PluginManager.getInstance()
+    when (intent.action) {
+      Intent.ACTION_PACKAGE_ADDED, Intent.ACTION_PACKAGE_INSTALL, Intent.ACTION_PACKAGE_REMOVED, Intent.ACTION_PACKAGE_CHANGED -> {
+        pm.reloadPlugins()
+      }
+    }
   }
 }
